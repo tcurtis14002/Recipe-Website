cc1b063a909272dbedd5d4b1089f4c90a2f37f4f
Tyler <willdev0110@gmail.com>
2023-01-11 15:59:16 -0700

    finalize nav link responsiveness

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const getElement = (selector) => {
   const element = document.querySelector(selector);
-  if (element) return element
+  if (element) return element;
   throw Error(
     ` Please double chekc your class names, there is no ${selector} class`
-  )
+  );
 };
 
-const links = getElement('.nav-links');
-const navBtnDOM = getElement('.nav-btn');
+const links = getElement(".nav-links");
+const navBtnDOM = getElement(".nav-btn");
 
-navBtnDOM.addEventListner('click', () => {
-  links.classList.toggle('show-links')
+navBtnDOM.addEventListener("click", () => {
+  links.classList.toggle("show-links");
 });
-
